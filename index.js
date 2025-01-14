9e1ec16d46301b458cba7f25a3be2b49d635f71f
MukitHossen7 <hossenmukit7@gmail.com>
2025-01-14 20:08:36 +0600

    add JWT authentication and role-based access control middleware

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { connection, client } = require("./DB/MongoDB");
 const app = express();
 const port = process.env.PORT || 5000;
 connection();
+
 //Middleware
 app.use(express.json());
 app.use(
@@ -14,10 +17,59 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
+app.use(cookieParser());
+//Middleware
+
+//Custom Middleware
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+const verifyAdmin = async (req, res, next) => {
+  const email = req.user.email;
+  const user = await usersCollection.findOne({ email });
+  if (!user || user.role !== "admin") {
+    return res.status(403).send({ message: "Forbidden" });
+  }
+  next();
+};
+//Custom Middleware
 
 //All Collection
 const usersCollection = client.db("hostelManagement").collection("users");
 //All Collection
+
+//Create token use jwt
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.JWT_SECRET_KEY, { expiresIn: "1h" });
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ success: true });
+});
+app.post("/logout", async (req, res) => {
+  res
+    .clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ success: true });
+});
+//Create token use jwt
 
 //Save all Users Data in Users Collection
 app.post("/users", async (req, res) => {
@@ -30,6 +82,18 @@ app.post("/users", async (req, res) => {
   res.send(result);
 });
 //Save all Users Data in Users Collection
+
+//get data from User Collection by role base
+app.get("/user/role/:email", async (req, res) => {
+  const email = req.params.email;
+  const user = await usersCollection.findOne({ email });
+  if (user) {
+    res.send(user.role);
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+//get data from User Collection by role base
 app.get("/", (req, res) => {
   res.send("Hostel Management System is running");
 });
